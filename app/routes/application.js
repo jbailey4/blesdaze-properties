1b3d02e1000c5dd799fbf9bbdcaee09f59ee016f
josh <jbailey0157@gmail.com>
2016-07-01 21:06:20 -0400

    updates content

--- app/routes/application.js
+++ app/routes/application.js
@@ -9,17 +9,4 @@ const {
 
 const { service } = inject;
 
-export default Route.extend(ApplicationRouteMixin, {
-  session: service('session'),
-
-  /*
-    NOTE: Temporary until site content is restructured by business owner
-
-    allow the logged in admins to see the public site, otherwise the splash page is shown
-  */
-  beforeModel(transition) {
-    if (!get(this, 'session.isAuthenticated') && transition.targetName !== 'login') {
-      this.transitionTo('splash');
-    }
-  }
-});
+export default Route.extend(ApplicationRouteMixin);
